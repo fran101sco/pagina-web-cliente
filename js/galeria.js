34414c02c006c447f1a4b37837d0b6d7c5b3d0c9
fran101sco <maldonadogarciafran@gmail.com>
2025-01-03 19:22:29 +0100

    galeria.js

--- js/galeria.js
+++ js/galeria.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
     const gallery = document.getElementById("gallery");
     const images = [
-        "../images/galeria/cinta.jpg",
-        "../images/galeria/cuerda.jpg",
-        "../images/galeria/grupo.jpg",
-        "../images/galeria/pesa.jpg",
-        "../images/galeria/estira.jpg",
-        "../images/galeria/comba.jpg"
+        "./images/galeria/cinta.jpg",
+        "./images/galeria/cuerda.jpg",
+        "./images/galeria/grupo.jpg",
+        "./images/galeria/pesa.jpg",
+        "./images/galeria/estira.jpg",
+        "./images/galeria/comba.jpg"
     ];
 
     images.forEach((src) => {
